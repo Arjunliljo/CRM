4f09b87b71bf82b10e10d478fb30aa75bd5506a7
marketLube <marketlubes@gmail.com>
2025-01-04 19:44:18 +0530

    created Lead

--- src/pages/leads/Leads.jsx
+++ src/pages/leads/Leads.jsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import AutoBtn from "../../components/buttons/AutoBtn";
 import SearchBar from "../../components/smallComponents/SearchBar";
+import LeadCard from "../../components/Card/LeadCard";
 
 export default function Leads() {
+  const [isLeads, setIsLeads] = useState(true);
   return (
     <main className="main-body leads">
       <div className="main-body-head">
@@ -10,10 +13,33 @@ export default function Leads() {
             <SearchBar />
             <AutoBtn>Auto Assign</AutoBtn>
           </div>
-          <div className="main-body-head-left-bottom"></div>
+          <div className="main-body-head-left-bottom">
+            <button onClick={() => setIsLeads((val) => !val)}>Leads</button>
+          </div>
         </div>
       </div>
-      <div className="main-body-box"></div>
+      <div className="main-body-box">
+        <div
+          className="main-body-box-left"
+          style={isLeads ? { width: "60%" } : { width: "100%" }}
+        >
+          <div className="main-body-scroll-container">
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+            <LeadCard />
+          </div>
+        </div>
+        <div
+          className="main-body-box-right"
+          style={isLeads ? { width: "40%" } : { width: "0%" }}
+        ></div>
+      </div>
     </main>
   );
 }
