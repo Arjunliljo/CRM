8c70faa97fd99ae458fb282ff59b6446405615ca
marketLube <marketlubes@gmail.com>
2025-01-16 05:56:52 -0800

    layout grid

--- src/layout/Dash.jsx
+++ src/layout/Dash.jsx
@@ -1,5 +1,17 @@
 function Dash() {
-  return <div></div>;
+  return (
+    <main className="dashbord-container">
+      <div className="dashbord-container-cards">
+        <div style={{ backgroundColor: "red", gridRow: "span 2" }}>Card 1</div>
+        <div style={{ backgroundColor: "red" }}>Card 2</div>
+        <div style={{ backgroundColor: "red" }}>Card 3</div>
+        <div style={{ backgroundColor: "red" }}>Card 4</div>
+        <div style={{ backgroundColor: "red" }}>Card 5</div>
+        <div style={{ backgroundColor: "red" }}>Card 6</div>
+        <div style={{ backgroundColor: "red" }}>Card 7</div>
+      </div>
+    </main>
+  );
 }
 
 export default Dash;
