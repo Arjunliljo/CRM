a68c22fe42aa51285871c06d822ae18b03049f47
marketLube <marketlubes@gmail.com>
2025-01-29 15:35:45 +0530

    ls

--- src/pages/Students/Students.jsx
+++ src/pages/Students/Students.jsx
@@ -17,24 +17,173 @@ import DocumentUpload from "../../components/smallComponents/DocumentUpload";
 import ModalBase from "../../components/Forms/ModalBase";
 import { useState } from "react";
 
-const student = {
-  num: 3,
-  name: "John Doe",
-  img: "https://via.placeholder.com/150",
-  number: 1234567890,
-  status: "Interested",
-  statusColor: "red",
-  remark:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.",
-  applications: 2,
-  attempts: 1,
-  country: "Germany",
-  count: 3,
-};
-const arr = [...Array(500)].map((_, i) => {
-  const obj = { ...student, _id: i };
-  return obj;
-});
+const names = [
+  "Aarav",
+  "Aaryan",
+  "Adarsh",
+  "Adithya",
+  "Advik",
+  "Akhil",
+  "Amal",
+  "Anand",
+  "Ananthu",
+  "Anish",
+  "Arjun",
+  "Arvind",
+  "Ashwin",
+  "Bhavana",
+  "Chithra",
+  "Dev",
+  "Dhanush",
+  "Deepak",
+  "Dileep",
+  "Eashan",
+  "Fahad",
+  "Gautham",
+  "Govind",
+  "Hari",
+  "Harikrishnan",
+  "Harsha",
+  "Indrajith",
+  "Jagadeesh",
+  "Jithin",
+  "Jeevan",
+  "Kannan",
+  "Krishna",
+  "Karthik",
+  "Lakshmi",
+  "Lekha",
+  "Madhavan",
+  "Mahesh",
+  "Maya",
+  "Meera",
+  "Mithun",
+  "Mohan",
+  "Nakul",
+  "Nandan",
+  "Narayan",
+  "Nayana",
+  "Nikhil",
+  "Niranjana",
+  "Parvathy",
+  "Pranav",
+  "Pranitha",
+  "Prashanth",
+  "Praveen",
+  "Preetha",
+  "Priya",
+  "Rahul",
+  "Rajeev",
+  "Rakesh",
+  "Ram",
+  "Ravi",
+  "Reshma",
+  "Revathi",
+  "Riya",
+  "Rohit",
+  "Saanvi",
+  "Sagar",
+  "Sandeep",
+  "Santhosh",
+  "Saranya",
+  "Sarath",
+  "Sharath",
+  "Siddharth",
+  "Sreeram",
+  "Suhas",
+  "Sumesh",
+  "Suraj",
+  "Surya",
+  "Swathi",
+  "Tara",
+  "Tejas",
+  "Ujwal",
+  "Usha",
+  "Ved",
+  "Vihaan",
+  "Vignesh",
+  "Vinay",
+  "Vineeth",
+  "Vinu",
+  "Vishnu",
+  "Vivek",
+  "Yash",
+  "Zara",
+  "Zoya",
+  "Akhila",
+  "Ananya",
+  "Arya",
+  "Avani",
+  "Diya",
+  "Ishaan",
+  "Ishika",
+  "Kabir",
+];
+
+const statuses = [
+  "Interested",
+  "Pending",
+  "Not Interested",
+  "Follow Up",
+  "Converted",
+  "Closed",
+  "New Lead",
+  "Attempted",
+  "In Progress",
+  "Prospect",
+  "Lost",
+  "More Info",
+  "Proposal Sent",
+  "Waiting",
+  "Appointment",
+  "Contact",
+  "Cold Lead",
+  "Hot Lead",
+  "Verified",
+  "Unverified",
+];
+
+const countries = [
+  "Germany",
+  "USA",
+  "India",
+  "UK",
+  "France",
+  "Italy",
+  "Brazil",
+  "Russia",
+  "China",
+  "Japan",
+  "Canada",
+  "Australia",
+  "Spain",
+  "Mexico",
+  "South Korea",
+  "South Africa",
+  "Netherlands",
+  "Sweden",
+  "Switzerland",
+  "Argentina",
+];
+const img =
+  "https://static.vecteezy.com/system/resources/thumbnails/036/594/092/small/man-empty-avatar-photo-placeholder-for-social-networks-resumes-forums-and-dating-sites-male-and-female-no-photo-images-for-unfilled-user-profile-free-vector.jpg";
+const arr = [...Array(100)].map((_, i) => ({
+  num: 1,
+  name: names[i % names.length], // Cycle through the names
+  img,
+  number: Math.floor(1000000000 + Math.random() * 9000000000), // Random 10-digit number
+  status: statuses[i % statuses.length], // Cycle through statuses
+  statusColor: ["red", "green", "blue", "brown"][i % 4], // Random color cycling
+  remark: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+  applications: Math.floor(Math.random() * 5) + 1, // Random between 1-5
+  attempts: Math.floor(Math.random() * 3) + 1,
+  country: countries[i % countries.length],
+  count: Math.floor(Math.random() * 5) + 1,
+  _id: i,
+  counsellor: "Aswathi S",
+  applicationHead: "Shruthi Hassan",
+  university: "SRM",
+}));
 
 export default function Students() {
   const { autoStudentsAssign, curStudent } = useSelector(
@@ -67,7 +216,9 @@ export default function Students() {
   const ISelectorOne = <Selector />;
   const ISelectorTwo = <Selector />;
   const ISelectorThree = <Selector />;
-  const IProfileCard = <ProfileCard IDocumentUpload={IDocumentUpload} />;
+  const IProfileCard = (
+    <ProfileCard IDocumentUpload={IDocumentUpload} student={curStudent} />
+  );
   const IStartApplication = <StartApplication />;
 
   const TopLeft = [<div key="search-bar">{ISearchBar}</div>];
