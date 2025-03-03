e2a56603113cee009c1a012a67a0d00e25f428ca
marketLube <marketlubes@gmail.com>
2025-03-03 15:04:54 +0530

    SL

--- config/socketConfig.js
+++ config/socketConfig.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Replace with your backend URL
-const SOCKET_SERVER_URL = "http://localhost:3000";
+const SOCKET_SERVER_URL = import.meta.env.VITE_SOCKET_SERVER_URL;
 
 // Initialize Socket connection
 const socket = io(SOCKET_SERVER_URL, {
